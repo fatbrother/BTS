1f9fffb636b8d881de83dad541f10a0096f68d55
fatbrother <kevin92422@gmail.com>
2025-05-06 00:05:53 +0800

    Add event details in constructor

--- contracts/ConcertTickets.sol
+++ contracts/ConcertTickets.sol
@@ -2,17 +2,33 @@
 pragma solidity ^0.8.17;
 
 contract ConcertTickets {
+    string public eventName;
     address public issuer;
     uint256 public totalTickets;
     uint256 public reservationStart;
     uint256 public reservationEnd;
     address[] public reservations;
-    mapping(address => bool) public reservationMap;
+    address[] public whitelists;
     mapping(address => uint256) public winners;
     mapping(uint256 => address) public ticketOwners;
 
-    constructor() {
+    constructor(
+        string memory _eventName,
+        uint256 _totalTickets,
+        uint256 _reservationStart,
+        uint256 _reservationEnd,
+        address[] memory _whitelists
+    ) {
+        require(_totalTickets > 0, "Total tickets must be greater than zero");
+        require(_reservationStart < _reservationEnd, "Invalid time range");
+        require(totalTickets == 0, "Event details already set");
+
+        eventName = _eventName;
         issuer = msg.sender;
+        totalTickets = _totalTickets;
+        reservationStart = _reservationStart;
+        reservationEnd = _reservationEnd;
+        whitelists = _whitelists;
     }
 
     modifier onlyIssuer() {
@@ -20,41 +36,38 @@ contract ConcertTickets {
         _;
     }
 
-    function setEventDetails(
-        uint256 _totalTickets,
-        uint256 _reservationStart,
-        uint256 _reservationEnd
-    ) external onlyIssuer {
-        require(_totalTickets > 0, "Total tickets must be greater than zero");
-        require(_reservationStart < _reservationEnd, "Invalid time range");
-        require(totalTickets == 0, "Event details already set");
-
-        totalTickets = _totalTickets;
-        reservationStart = _reservationStart;
-        reservationEnd = _reservationEnd;
+    modifier onlyWhitelisted() {
+        require(msg.sender == issuer || isWhitelisted(msg.sender), "Not whitelisted");
+        _;
     }
 
-    function reserveTicket() external {
+    function isWhitelisted(address _address) public view returns (bool) {
+        for (uint256 i = 0; i < whitelists.length; i++) {
+            if (whitelists[i] == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function reserveTicket() external onlyWhitelisted {
         require(
             block.timestamp >= reservationStart,
             "Reservation period not started"
         );
         require(block.timestamp <= reservationEnd, "Reservation period ended");
-        require(!reservationMap[msg.sender], "Already reserved");
+        for (uint256 i = 0; i < reservations.length; i++) {
+            require(reservations[i] != msg.sender, "Already reserved");
+        }
 
         reservations.push(msg.sender);
-        reservationMap[msg.sender] = true;
     }
 
-    function getReservations() external view returns (address[] memory) {
-        return reservations;
-    }
-
-    function endReservation() public onlyIssuer {
+    function endReservation() external onlyIssuer {
         reservationEnd = block.timestamp;
     }
 
-    function selectWinners(uint256 randomSeed) public onlyIssuer {
+    function selectWinners(uint256 randomSeed) external onlyIssuer {
         require(block.timestamp > reservationEnd, "Reservation period not ended");
         require(reservations.length > 0, "No reservations");
         require(totalTickets > 0, "No tickets to distribute");
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,26 @@
 const hre = require("hardhat");
 
 async function main() {
+    totalTickets = 100;
+    startTime = Math.floor(Date.now() / 1000);
+    endTime = startTime + 86400;
+
+    const Account = await hre.ethers.getSigners();
+    const whitelist = [
+        Account[0].address,
+        Account[1].address,
+        Account[2].address,
+        Account[3].address,
+        Account[4].address,
+    ];
     const ConcertTickets = await hre.ethers.getContractFactory("ConcertTickets");
-    const concertTickets = await ConcertTickets.deploy();
+    const concertTickets = await ConcertTickets.deploy(
+        "Concert Ticket",
+        totalTickets,
+        startTime,
+        endTime,
+        whitelist
+    );
     const address = await concertTickets.getAddress();
 
     console.log("ConcertTickets deployed to:", address);
